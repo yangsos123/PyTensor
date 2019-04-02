6b89abffb47cb94258e434306e5cb57cd4f15dd0
yangsos123 <connanmi1@qq.com>
2019-04-02 13:52:43 +0200

    test

--- programs/CheMPS.py
+++ programs/CheMPS.py
@@ -1,3 +1,7 @@
+import Contractor as ct
+import Spin as Sp
+import MPO
+import MPS
 import sys
 import random
 import numpy as np
@@ -6,33 +10,27 @@ import copy
 
 sys.path.append("../core")
 sys.path.append("../models")
-import MPS
-import MPO
-import Spin as Sp
-import Contractor as ct
 
 
-def ChebyTn(H, M, D, mode, input, saveT = False):
-	# input is an MPS if mode=="LDoS" and a list of MPOs if mode=="DoS"
-	L = H.L
-	s = H.s
-	mu = np.zeros((M))
-	if mode == "DoS":
-		Tm = MPO.MPO(L, 1, s)
-		Tm.setProductOperator(np.identity(s[0]))
-		Tm = MPO.MPSfromMPO(Tm)
-		Hp = MPO.extendMPO(H)
-		
-	elif mode == "LDoS":
-		Tm = input
-		Hp = copy.deepcopy(H)
-	else:
-		print("Mode not supported!")
-		exit(3)
+def ChebyTn(H, M, D, mode, input, saveT=False):
+    # input is an MPS if mode=="LDoS" and a list of MPOs if mode=="DoS"
+    L = H.L
+    s = H.s
+    mu = np.zeros((M))
+    if mode == "DoS":
+        Tm = MPO.MPO(L, 1, s)
+        Tm.setProductOperator(np.identity(s[0]))
+        Tm = MPO.MPSfromMPO(Tm)
+        Hp = MPO.extendMPO(H)
 
-	T = ct.fitApplyMPO(Hp, Tm, D)
+    elif mode == "LDoS":
+        Tm = input
+        Hp = copy.deepcopy(H)
+    else:
+        print("Mode not supported!")
+        exit(3)
 
+    T = ct.fitApplyMPO(Hp, Tm, D)
+    mu[0] = 1
 
-
-
-	return 0
+    return 0
